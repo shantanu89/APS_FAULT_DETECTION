61ea95e64d0cbfb05b1bde15b2917f0dbe1b1ce3
shantanu89 <shantanupradhan89@gmail.com>
2023-01-10 22:41:27 +0530

    .env file added

--- sensor/config.py
+++ sensor/config.py
@@ -1,5 +1,11 @@
 import pymongo
 import pandas as pd
 import json
-
+from dataclasses import dataclass
 #provide mongo db local host url to connect with mongo db
+
+class EnvironmentVariable:
+    mongodb_url:str=os.getenv("MONGO_DB_URK")
+
+
+client=pymongo.MongoClient("mongodb://localhost:27017")
