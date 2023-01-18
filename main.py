bb7e50cba31b78c36a41bcedb6db50172a99b768
shantanu89 <shantanupradhan89@gmail.com>
2023-01-18 09:09:59 +0530

    updated

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ if __name__=="__main__":
           data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          print(data_ingestion.ginitiate_data_ingestion())
      except Exception as e:
           print(e)
